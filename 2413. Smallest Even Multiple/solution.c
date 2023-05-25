bcedccd045b5f259ec151e5d015fd50720b33d91
HarishVijayV <harishvijay20042004@gmail.com>
2023-05-25 21:03:20 +0530

    updated

---
+++ 2413. Smallest Even Multiple/solution.c
@@ -0,0 +1,21 @@
+int smallestEvenMultiple(int n){
+      if (n==1){
+        return 2;
+      }
+      for(int i=1;i<=n/2;i++){
+       for(int j=1; j<=n;j++) {
+            if (n*i==2*j){
+              
+            return n*i;    
+      } 
+      else{
+        continue;
+      }
+    
+      }
+     
+    }
+    return 6;
+
+
+}
