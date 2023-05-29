0dded8d4c3c219592077f4ba5f055c0dd097877d
HarishVijayV <harishvijay20042004@gmail.com>
2023-05-29 18:22:07 +0530

    updated

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int org,s=0,p=1;
+        while(n>0){
+            org=n%10;
+            s=s+org;
+            p=p*org;
+            n=n/10;
+
+        }
+        return p-s;
+    }
+}
