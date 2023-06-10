6acdd742ce0ce1ccb01799c247716a8f338a7394
HarishVijayV <harishvijay20042004@gmail.com>
2023-06-10 11:50:47 +0530

    updated

--- 231. Power of Two/solution.c
+++ 231. Power of Two/solution.c
@@ -1,9 +1,8 @@
 bool isPowerOfTwo(int n){
-    if (n <= 0) {
-        return false;
-    }
-        while (n%2==0) {
-        n/=2;
-    }
-        return n==1;
+    if(n<=0){
+            return false;
+        }    
+        if ((n & (n - 1)) == 0) {
+            return true;}
+            return false;
 }
--- 231. Power of Two/solution.java
+++ 231. Power of Two/solution.java
@@ -1,11 +1,10 @@
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-    if (n <= 0) {
-        return false;
-    }
-        while (n%2==0) {
-        n/=2;
-    }
-        return n==1;
+    public boolean isPowerOfTwo(int n) {  
+        if(n<=0){
+            return false;
+        }    
+        if ((n & (n - 1)) == 0) {
+            return true;}
+            return false;
     }
 }
