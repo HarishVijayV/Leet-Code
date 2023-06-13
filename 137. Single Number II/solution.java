0c1401378a5b3a7981ab4dbeeaeabd09c8e18b7f
HarishVijayV <harishvijay20042004@gmail.com>
2023-06-13 12:37:31 +0530

    Updated

---
+++ 137. Single Number II/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int count=0;
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                count++;
+            }
+            else{
+                if(count<2){
+            return nums[i-1];
+        }
+        count=0;
+    }
+}
+        return nums[nums.length-1];
+    }
+}
+
+
