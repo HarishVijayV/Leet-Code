0cbe965b0814451384a0eccfb5062ce4ffe0278e
HarishVijayV <harishvijay20042004@gmail.com>
2023-06-22 09:29:12 +0530

    updated

--- 1051. Height Checker/solution.java
+++ 1051. Height Checker/solution.java
@@ -1,10 +1,7 @@
 class Solution {
     public int heightChecker(int[] heights) {
         int n=heights.length,count=0;
-        int [] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=heights[i];
-        }
+        int [] arr=heights.clone();
         Arrays.sort(heights);
         for(int i=0;i<n;i++){
             if(arr[i]!=heights[i]){
