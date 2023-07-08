4ec34e3d12d79024694a2dd7fa40acb94bd2291d
HarishVijayV <harishvijay20042004@gmail.com>
2023-07-08 11:13:08 +0530

    updated

---
+++ 1688. Count of Matches in Tournament/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int j=0;
+        while(n>1){
+            if(n%2==0){
+                j+=n/2;
+                n=n/2;
+            }
+            else{
+                j+=(n-1)/2;
+                n=((n-1)/2)+1;
+            }
+        }
+        return j;
+    }
+}
