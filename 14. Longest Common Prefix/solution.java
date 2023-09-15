f97ad151dae00aa242442883ab693fded53d1e83
HarishVijayV <harishvijay20042004@gmail.com>
2023-09-15 10:20:59 +0530

    updated

---
+++ 14. Longest Common Prefix/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String a,b,out="";
+        int j;
+        Arrays.sort(strs);
+        a=strs[0];
+        b=strs[strs.length-1];
+        j=Math.min(a.length(),b.length());
+        for(int i=0;i<j;i++){
+            if (a.charAt(i)==b.charAt(i)){
+                out+=(a.charAt(i));
+            }
+            else{
+                break;
+            }    
+        }
+        return out;
+    }
+}
