1ee68c27b817a5c81d9ed7ebb23995446a3d62c5
HarishVijayV <harishvijay20042004@gmail.com>
2023-09-28 10:26:22 +0530

    updated

---
+++ 2000. Reverse Prefix of Word/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        """
+        :type word: str
+        :type ch: str
+        :rtype: str
+        """
+        s=word
+        for i in range(len(word)):
+            if word[i]==ch:
+                s=(word[:i+1])
+                s=s[::-1]
+                s+=word[i+1:]
+                break
+        return s
+
+
+        
