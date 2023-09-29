f0851b281cfd32867c8ad04edcc483f20dfd077d
HarishVijayV <harishvijay20042004@gmail.com>
2023-09-29 07:01:15 +0530

    updated

--- 9. Palindrome Number/solution.java
+++ 9. Palindrome Number/solution.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int org=x;
-        int rev=0;
+        int reve=0;
         if(x<0){
             return false;
         }
         else{
             while (x > 0) {
                 int digit=x%10;
-                rev=rev*10+digit;
+                reve=reve*10+digit;
                 x=x/10;
             }
         }
-        if(rev==org){
+        if(reve==org){
             return true;
         }
         return false;
-
     }
 }
