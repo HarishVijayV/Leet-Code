b32008fd9111bbdc87aaac75b407d93365a9b295
HarishVijayV <harishvijay20042004@gmail.com>
2024-01-08 15:42:00 +0530

    updated

---
+++ 2553. Separate the Digits in an Array/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        res=[]
+        for i in nums:
+            for j in str(i):
+                res.append(int(j))
+        return res
+        
