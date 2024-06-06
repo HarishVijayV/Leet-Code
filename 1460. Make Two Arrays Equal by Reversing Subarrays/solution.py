9f6b47b74b2248219a5a02c4f7e38c09eeba7692
HarishVijayV <harishvijay20042004@gmail.com>
2024-06-06 13:20:35 +0530

    updated

---
+++ 1460. Make Two Arrays Equal by Reversing Subarrays/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        a=sorted(target)
+        b=sorted(arr)
+        if a==b:
+            return True
+        else:
+            return False
+
+        
