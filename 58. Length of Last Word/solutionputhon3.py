47d4063b16b3443792845c72f30f2460bd67b6fe
HarishVijayV <harishvijay20042004@gmail.com>
2024-07-30 18:53:55 +0530

    updated

---
+++ 58. Length of Last Word/solutionputhon3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        c=0
+        for i in range(len(s)-1,-1,-1):
+            if s[i]==' ':
+                continue
+            else:
+                c+=1
+                if s[i-1]==' ':
+                    return c
+        return c
+
+            
+        
