9ae41395b4356c1806f6e3f2433dac6800db0a3d
Harish Vijay <121938589+HarishVijayV@users.noreply.github.com>
2025-07-15 15:26:14 +0530

    Time: 2 ms (96.71%), Space: 42 MB (19.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> my = new Stack<>();
+        for(char x:s.toCharArray()){
+            if ((x=='{')||(x=='(')||(x=='[')){
+                my.push(x);
+            }
+            if(!my.empty()){
+                if(x=='}'){
+                    if (my.peek()=='{'){
+                        my.pop();
+                    }
+                    else{
+                        my.push(x);
+                    }
+                }
+                else if(x==')'){
+                    if (my.peek()=='('){
+                        my.pop();
+                    }
+                    else{
+                        my.push(x);
+                    }
+                }
+                else if(x==']'){
+                    if (my.peek()=='['){
+                        my.pop();
+                    }
+                    else{
+                        my.push(x);
+                    }
+                }
+            }else{
+                my.push(x);
+            }
+        } 
+        if (my.empty()){
+            return true;
+        }
+        return false;
+    }
+}
